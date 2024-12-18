e0f71b7feaff20deef2e9ad22ef3669a110935dc
niklaskahr <mail@niklaskahr.com>
2024-12-18 09:29:32 +0100

    feat: enable local storage of analyzed emails

--- src/app/services/shared/event.service.ts
+++ src/app/services/shared/event.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { EmailData } from '../../shared/interfaces/email-data.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
   private filesDroppedSubject = new Subject<File[]>();
+  private analysisStoredSubject = new Subject<EmailData>();
   filesDropped$ = this.filesDroppedSubject.asObservable();
+  analysisStored$ = this.analysisStoredSubject.asObservable();
 
   notifyFilesDropped(files: File[]): void {
     this.filesDroppedSubject.next(files);
+  }
+
+  notifyAnalysisStored(data: EmailData): void {
+    this.analysisStoredSubject.next(data);
   }
 }
--- src/app/services/shared/storage.service.ts
+++ src/app/services/shared/storage.service.ts
@@ -1,22 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { HashingUtil } from '../../shared/utils/hashing.util';
 import { EmailData } from '../../shared/interfaces/email-data.interface';
+import { isPlatformBrowser } from '@angular/common';
+import { EventService } from './event.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
+  private readonly LOCAL_STORAGE_KEY = 'emailAnalyses';
   private emailHashes: Set<string> = new Set();
-  private dataStore: Map<string, EmailData> = new Map(); // also store raw email body in EmailData?
+  private dataStore: Map<string, EmailData> = new Map();
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private eventService: EventService
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadFromLocalStorage();
+    }
+  }
 
   async storeData(data: EmailData): Promise<void> {
-    const hash = await HashingUtil.generateHash(data.email);
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    const hash = await HashingUtil.generateHash(data.email); // generate a hash to detect duplicates
 
     if (!this.emailHashes.has(hash)) {
       this.emailHashes.add(hash);
       this.dataStore.set(hash, data);
+      this.saveToLocalStorage();
+      this.eventService.notifyAnalysisStored(data);
     } else {
-      console.warn('Duplicate data detected. Skipping storage.', data);
+      console.warn('Duplicate email detected. Skipping storage.'); // TODO: notify user
     }
   }
 
@@ -24,7 +40,25 @@ export class StorageService {
     return Array.from(this.dataStore.values());
   }
 
-  getDataByHash(hash: string): EmailData | undefined {
-    return this.dataStore.get(hash);
+  // getDataByHash(hash: string): EmailData | undefined {
+  //   return this.dataStore.get(hash);
+  // }
+
+  private saveToLocalStorage(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    const serializedData = JSON.stringify(Array.from(this.dataStore.entries()));
+    // localStorage.setItem(this.LOCAL_STORAGE_KEY, serializedData);
+  }
+
+  private loadFromLocalStorage(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    const savedData = localStorage.getItem(this.LOCAL_STORAGE_KEY);
+    if (savedData) {
+      const parsedEntries: [string, EmailData][] = JSON.parse(savedData);
+      this.dataStore = new Map(parsedEntries);
+      this.emailHashes = new Set(parsedEntries.map(([hash]) => hash));
+    }
   }
 }
