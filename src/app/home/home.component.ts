f3ee1daa7eef221650c848cf4d02cf535643bc39
niklaskahr <mail@niklaskahr.com>
2024-12-18 09:30:38 +0100

    refactor: integrate report component into home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { DropZoneComponent } from '../drop-zone/drop-zone.component';
-
+import { DropZoneComponent } from './drop-zone/drop-zone.component';
+import { ReportComponent } from './report/report.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DropZoneComponent],
+  imports: [DropZoneComponent, ReportComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
